1a1f644a1f1ac836a5d01724a91ca0cca322b76b
magicianLucas <lucastelesieqs@gmail.com>
2024-07-31 10:10:27 -0300

    Atualização de exemplos

--- NodeGeracaoTech/node04/carro.js
+++ NodeGeracaoTech/node04/carro.js
@@ -1,9 +1,28 @@
-const Veiculo = require('NodeGeracaoTech/node04/veiculos.js')
+// const Veiculo = require('NodeGeracaoTech/node04/veiculos.js')
+
+// class Carro extends Veiculo{
+//     constructor(marca, modelo, anoFabricacao, preco, infos, qtdPorta, tipoCombustivel){
+//         super(marca, modelo, anoFabricacao, preco, infos )
+//         this.qtdPorta = qtdPorta
+//         this.tipoCombustivel= tipoCombustivel
+//     }
+// }
+
+const Veiculo = require('./Veiculo');
 
 class Carro extends Veiculo{
-    constructor(marca, modelo, anoFabricacao, preco, infos, qtdPorta, tipoCombustivel){
-        super(marca, modelo, anoFabricacao, preco, infos )
-        this.qtdPorta = qtdPorta
-        this.tipoCombustivel= tipoCombustivel
+    constructor(marca, modelo, ano, preco, qtdPortas, tipoCombustivel){
+        super(marca, modelo, ano, preco);
+        this.qtdPortas = qtdPortas;
+        this.tipoCombustivel = tipoCombustivel;
     }
+
+    exibirInformacoes(){
+        //sobrecarga
+        super.exibirInformacoes();
+        console.log(`Quantidade de portas: ${this.qtdPortas}, Tipo de combustível: ${this.tipoCombustivel}`);
+    }
+
 }
+
+module.exports = Carro;
--- NodeGeracaoTech/node04/moto.js
+++ NodeGeracaoTech/node04/moto.js
@@ -1,9 +1,27 @@
-const Veiculo = require('NodeGeracaoTech/node04/veiculos.js')
+// const Veiculo = require('NodeGeracaoTech/node04/veiculos.js')
+
+// class Moto extends Veiculo{
+//     constructor(marca, modelo, anoFabricacao, preco, infos, cilindrada, tipoPartida){
+//         super(marca, modelo, anoFabricacao, preco, infos)
+//         this.cilindrada = cilindrada;
+//         this.tipoPartida = tipoPartida;
+//     }
+// }
+
+const Veiculo = require('./Veiculo');
 
 class Moto extends Veiculo{
-    constructor(marca, modelo, anoFabricacao, preco, infos, cilindrada, tipoPartida){
-        super(marca, modelo, anoFabricacao, preco, infos)
-        this.cilindrada = cilindrada;
-        this.tipoPartida = tipoPartida;
+    constructor(marca, modelo, ano, preco, cilindradas, partida){
+        super(marca, modelo, ano, preco); //Veículo
+        this.cilindradas = cilindradas;
+        this.partida = partida;
     }
+
+    exibirInformacoes(){
+        //sobreEscrita
+        console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}, Preço: ${this.getPreco()}, Cilindradas: ${this.cilindradas}, Partida: ${this.partida}`);
+    }
+
 }
+
+module.exports = Moto;
